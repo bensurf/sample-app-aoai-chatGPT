b80bcab23b519b4c9ba8772c2960f3b60d815ada
toporide <benfilip@protonmail.com>
2025-02-11 14:49:08 -0800

    begin

--- app.py
+++ app.py
@@ -113,64 +113,65 @@ MS_DEFENDER_ENABLED = os.environ.get("MS_DEFENDER_ENABLED", "true").lower() == "
 
 # Initialize Azure OpenAI Client
 async def init_openai_client():
-    azure_openai_client = None
+    #azure_openai_client = None
+    azure_deepseek_client = None
     
     try:
         # API version check
-        if (
-            app_settings.azure_openai.preview_api_version
-            < MINIMUM_SUPPORTED_AZURE_OPENAI_PREVIEW_API_VERSION
-        ):
-            raise ValueError(
-                f"The minimum supported Azure OpenAI preview API version is '{MINIMUM_SUPPORTED_AZURE_OPENAI_PREVIEW_API_VERSION}'"
-            )
+        # if (
+        #     app_settings.azure_openai.preview_api_version
+        #     < MINIMUM_SUPPORTED_AZURE_OPENAI_PREVIEW_API_VERSION
+        # ):
+        #     raise ValueError(
+        #         f"The minimum supported Azure OpenAI preview API version is '{MINIMUM_SUPPORTED_AZURE_OPENAI_PREVIEW_API_VERSION}'"
+        #     )
 
-        # Endpoint
-        if (
-            not app_settings.azure_openai.endpoint and
-            not app_settings.azure_openai.resource
-        ):
-            raise ValueError(
-                "AZURE_OPENAI_ENDPOINT or AZURE_OPENAI_RESOURCE is required"
-            )
+        # # Endpoint
+        # if (
+        #     not app_settings.azure_openai.endpoint and
+        #     not app_settings.azure_openai.resource
+        # ):
+        #     raise ValueError(
+        #         "AZURE_OPENAI_ENDPOINT or AZURE_OPENAI_RESOURCE is required"
+        #     )
 
-        endpoint = (
-            app_settings.azure_openai.endpoint
-            if app_settings.azure_openai.endpoint
-            else f"https://{app_settings.azure_openai.resource}.openai.azure.com/"
-        )
+        # endpoint = (
+        #     app_settings.azure_openai.endpoint
+        #     if app_settings.azure_openai.endpoint
+        #     else f"https://{app_settings.azure_openai.resource}.openai.azure.com/"
+        # )
 
         # Authentication
-        aoai_api_key = app_settings.azure_openai.key
-        ad_token_provider = None
-        if not aoai_api_key:
-            logging.debug("No AZURE_OPENAI_KEY found, using Azure Entra ID auth")
-            async with DefaultAzureCredential() as credential:
-                ad_token_provider = get_bearer_token_provider(
-                    credential,
-                    "https://cognitiveservices.azure.com/.default"
-                )
+        deepseek_api_key = app_settings.azure_openai.key
+        #ad_token_provider = None
 
         # Deployment
-        deployment = app_settings.azure_openai.model
-        if not deployment:
-            raise ValueError("AZURE_OPENAI_MODEL is required")
+        #deployment = app_settings.azure_openai.model
+        #if not deployment:
+        #    raise ValueError("AZURE_OPENAI_MODEL is required")
 
         # Default Headers
-        default_headers = {"x-ms-useragent": USER_AGENT}
+        #default_headers = {"x-ms-useragent": USER_AGENT}
 
-        azure_openai_client = AsyncAzureOpenAI(
-            api_version=app_settings.azure_openai.preview_api_version,
-            api_key=aoai_api_key,
-            azure_ad_token_provider=ad_token_provider,
-            default_headers=default_headers,
-            azure_endpoint=endpoint,
-        )
+        # azure_openai_client = AsyncAzureOpenAI(
+        #     api_version=app_settings.azure_openai.preview_api_version,
+        #     api_key=aoai_api_key,
+        #     azure_ad_token_provider=ad_token_provider,
+        #     default_headers=default_headers,
+        #     azure_endpoint=endpoint,
+        # )
 
-        return azure_openai_client
+        from azure.ai.inference import ChatCompletionsClient
+        from azure.core.credentials import AzureKeyCredential
+        azure_deepseek_client = ChatCompletionsClient(
+            endpoint='https://DeepSeek-R1-ngtya.westus.models.ai.azure.com',
+            credential=AzureKeyCredential(deepseek_api_key)
+)
+
+        return azure_deepseek_client
     except Exception as e:
-        logging.exception("Exception in Azure OpenAI initialization", e)
-        azure_openai_client = None
+        logging.exception("Exception in Azure Deepseek initialization", e)
+        azure_deepseek_client = None
         raise e
 
 
